b9180cdc7a0c5fed34ca78a90e782f71eb1bc545
Bluscream <admin@timo.de.vc>
2018-04-24 19:09:10 +0200

    Add project files.

---
+++ TS3 Dynamic Channel Creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS3_Dynamic_Channel_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
